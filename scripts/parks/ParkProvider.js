d29be6b1371157c7a344b0e23ec46f060feeb213
wggreen <wggreen.nss@gmail.com>
2019-12-17 13:09:30 -0600

    working on weather component

--- scripts/parks/ParkProvider.js
+++ scripts/parks/ParkProvider.js
@@ -9,7 +9,7 @@ export const useParks= () => {
 // DECLARE AND EXPORT A FUNCTION CALLED GETPARKS THAT FETCHES OUR API DATA, MAKES IT ITERABLE.
 // THEN POPULATES IT AS THE CONTENT OUR OUR ORIGINAL ARRAY CALLED PARKS
 export const getParks = () => {
-  return fetch('https://developer.nps.gov/api/v1/parks?api_key=pa5r3mOGT93Qnoyobsb4n3iv6xiDH1hrOtXwFLOw&fields=images')
+  return fetch('https://developer.nps.gov/api/v1/parks?api_key=pa5r3mOGT93Qnoyobsb4n3iv6xiDH1hrOtXwFLOw&fields=images&fields=addresses')
   .then(response => response.json()
   )
   .then(
---
+++ scripts/weather/WeatherComponent.js
@@ -0,0 +1,10 @@
+const WeatherComponent = (weather) => {
+    date = weather.sys.dt_txt.split(" ")
+    return `
+        <section class="weather_component">
+            
+        </section>
+    `
+}
+
+export default NoteHTML
