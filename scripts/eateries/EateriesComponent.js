1d7a00646ca3909277e2e02aa4129696baaeaefb
Coffey May <jcoffeymay6@gmail.com>
2019-12-17 15:57:54 -0600

    eateries info displaying

--- scripts/eateries/EateriesComponent.js
+++ scripts/eateries/EateriesComponent.js
@@ -3,14 +3,17 @@ const EateryComponent = (eatery) => {
 
     return `
     <section class="eatery-card">
-    <header class="eateryFullName">${eatery.fullname}</header>
+    <header class="eateryFullName">${eatery.businessName}</header>
 <br>
 <div class ="eateryInformation">
   <ul>
-  <li>${eatery.description}</li>
-  <li>${eatery.state}</li>
-  <li>${eatery.city}</li>
-  <li>${eatery.amenities}</li>
+  <li>DESCRIPTION:${eatery.description}</li>
+  <li>STATE:${eatery.state}</li>
+  <li>CITY:${eatery.city}</li>
+  <li>PET-FRIENDLY:${eatery.ameneties.petFriendly}</li>
+  <li>WIFI:${eatery.ameneties.wifi}</li>
+  <li>RESTROOMS:${eatery.ameneties.restrooms}</li>
+  
   </ul>
    </div>
    </section>
