063fa4b1af481d0335dc3c781189c3ca56aed5c9
Mitali Vasisht <vamdev37@gmail.com>
2019-12-18 15:17:03 -0600

    new master

--- scripts/attractions/AttractionComponent.js
+++ scripts/attractions/AttractionComponent.js
@@ -6,17 +6,17 @@ const attractionComponent = (attraction) => {
     <header class="attractionFullName">${attraction.name}</header>
 <br>
 <div class ="attractionInformation">
-  <ul>
-  <li>${attraction.description}</li>
-  <li>${attraction.state}</li>
-  <li>${attraction.city}</li>
-  <li>${attraction.ameneties.restrooms}</li>
-  </ul>
    </div>
    <div class="attractionDialogButton">
-            <button class="attractionDialogButton" id="attraction--${attraction.id}">Additional Park Details</button>
+            <button class="attractionDialogButton" id="attraction--${attraction.id}">Additional Details</button>
             <dialog class="dialog--button" id="details--${attraction.id}">
                 <div>More Details</div>
+                <ul>
+                <li>${attraction.description}</li>
+                <li>${attraction.state}</li>
+                <li>${attraction.city}</li>
+                <li>${attraction.ameneties.restrooms}</li>
+                </ul>
                         <div>Description:</div>
                         <div>Events:</div>
                 <button class="button--close">Close Details</button>
--- scripts/main.js
+++ scripts/main.js
@@ -35,6 +35,8 @@ getAttractions ()
     
 )
 
+
+
 getWeather()
 .then(
     () => WeatherFilter()
