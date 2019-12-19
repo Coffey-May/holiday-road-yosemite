24d68e17744c0b77065ca1a8fdc51924badaf654
kevinpenny3 <kevin.penny3@yahoo.com>
2019-12-19 14:48:13 -0600

    some css

--- scripts/attractions/AttractionComponent.js
+++ scripts/attractions/AttractionComponent.js
@@ -4,11 +4,11 @@ const attractionComponent = (attraction) => {
     return `
     <section class="attraction-card">
     <header class="attractionFullName">${attraction.name}</header>
-<br>
-<div class="attractionImg">
-<img src="https://www.planetware.com/photos-large/USWY/usa-wyoming-yellowstone-national-park.jpg" alt="">
-</div>
-<div class ="attractionInformation">
+    <br>
+    <div class="attractionImg">
+    <img src="https://www.planetware.com/photos-large/USWY/usa-wyoming-yellowstone-national-park.jpg" alt="">
+    </div>
+    <div class ="attractionInformation">
    </div>
    <div class="attractionDialogButton">
             <button class="attractionDialogButton" id="attraction--${attraction.id}">Additional Details</button>
